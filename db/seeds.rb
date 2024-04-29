12f24f1f56c5b45a5f45c44837fbe9b7ea3f6971
Phillip Strom <phillip.strom@gmail.com>
2024-04-29 11:44:36 -0600

    Add seeds from fixture files

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Seeding the database with fixture quotes..."
+system("bin/rails db:fixtures:load")
